9f598ef692750fb7203649e5364543b1fb8b3538
Gi0Gu3z <71970505+Gi0Gu3z@users.noreply.github.com>
2020-09-27 15:05:00 +0200

    añadido toda la practica

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
+var domain = 'yahoo.com'
 var getRandomEmailForDomain = function(domain) {
-    // Tip 1: Use the variable domain (this is your input), not exampleDomainName!
    var NameMail = Math.random().toString(36).substring(7);
    return NameMail + "@" + domain;
-    // Tip 2: use the following to generate a random string:
-    // Math.random().toString(36).substring(7); This will output something like: hfj56s
 }
