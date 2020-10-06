808b6e03393b426bad1a7036b55b2c53582fb25c
Gi0Gu3z <sergiodominguezescorial@gmail.com>
2020-10-06 11:33:20 +0200

    añadidos los tests

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-var domain = 'yahoo.com'
+var domain = 'yahoo.com';
 var getRandomEmailForDomain = function(domain) {
    var NameMail = Math.random().toString(36).substring(7);
    return NameMail + "@" + domain;
 } 
+
+
+
+
+let i;
+for ( i = 1; i < 4; i++) {
+  pm.environment.set("boardName", "My Board " + i);
+}
+
+
+
+
+
+function name(params) {
+   
+}
