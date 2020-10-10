b448383788de1298059e605adc7b83615dc56e91
Gi0Gu3z <sergiodominguezescorial@gmail.com>
2020-10-10 21:15:15 +0200

    assertions filter added

--- index.js
+++ index.js
@@ -12,10 +12,55 @@ for ( i = 1; i < 4; i++) {
   pm.environment.set("boardName", "My Board " + i);
 }
 
+Math.random().toString
 
+for ()
 
-
-
-function name(params) {
-   
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  
 }
+
+function name(params) { 
+  
+pm.test. 
+
+
+}
+
+
+
+
+
+
+let jsonData = pm.respone.json();
+for (let filter of jsonData.filter) {
+  if (jsonData.filter.name == "MANUFACTURER") {
+   let filter = jsonData.filter;
+  }
+  
+  pm.test("Your test name", function () {
+  filter.name.to.eqal("MANUFACTURER");
+  filter.isAllowed.to.be.true;
+
+  })
+
+
+}
+
+
+
+
+
+let jsonData = pm.response.json();
+let manufacturer;
+for (let filter of jsonData.filters) {
+  if (filter.name == "MANUFACTURER") {
+   manufacturer = filter;
+   console.log(manufacturer);
+  }
+}
+  pm.test("Your test name", function () {
+  pm.expect(manufacturer.name).to.eql("MANUFACTURER");
+  pm.expect(manufacturer.isAllowed).to.be.false;
+  })
